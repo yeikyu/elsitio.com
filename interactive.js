c48d335c3f2e4ff11e2276d392e456784c16a106
Javier Quinteros <jquinte1998@gmail.com>
2021-06-04 03:21:26 -0300

    pagina con git

---
+++ interactive.js
@@ -0,0 +1,15 @@
+'use strict';
+let fecha = new Date();
+let hora = fecha.getHours().toString();
+let minutos=fecha.getMinutes().toString();
+let segundos= fecha.getSeconds().toString();
+console.log(hora+':'+minutos+':'+segundos);
+//titulo = document.getElementById('title');
+//titulo.style.border = '1px solid green';
+//window.addEventListener('load',()=> {
+  //  var tiempo = setInterval( ()=> {
+        
+ //   document.write('este mensaje se mostrara cada 3s');
+//},3000);
+//});
+
